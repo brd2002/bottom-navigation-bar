4d36f0201da74800903bea38b432bb7fab064c0f
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-27 13:47:01 +0530

    not doing good in android

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bottomnavigationbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.bottomnavigationbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNavigation = binding.bottomNavigationView
+        val bottombar  = findViewById<View>(R.id.fragmentContainerView)
+        bottomNavigation.setupWithNavController(bottombar.findNavController())
     }
 }
